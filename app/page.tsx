6c253a3d89f88c072e22ee8df934f026e4d44ca3
Yash Raj Varma <yashrajvarma9@gmail.com>
2025-05-01 01:21:12 +0530

    Made basic design

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-300">
       <h1 className="text-4xl font-bold mb-4">PaynPrint</h1>
-      <p className="text-lg mb-4">
+      <p className="text-lg mb-4 text-center">
         Your one-stop solution for printing documents with ease.
       </p>
-      <p className="text-lg mt-4">
+      <p className="text-lg mt-4 text-center">
         Upload your documents and let us handle the printing for you.
       </p>
       <div className="mt-8">
@@ -30,7 +30,7 @@ export default function Home() {
           Upload Document
           {isLoading && (
             <div>
-              <Spinner color="white" size="sm"/>
+              <Spinner color="white" size="sm" />
             </div>
           )}
         </Button>
