1cef703e2c643bc1aec54575633f21800a81415e
Yash Raj Varma <yashrajvarma9@gmail.com>
2025-06-29 01:15:42 +0530

    Initial Payment Test

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,11 +1,30 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 // import { useRouter } from "next/navigation";
 import {
   Button,
   Input,
 } from "@heroui/react";
 import EastIcon from "@mui/icons-material/East";
+import axios from "axios";
+
+
+interface PaymentData {
+  orderId: string;
+  state: string;
+  expiresAt: number;
+  redirectUrl: string;
+}
+interface PaymentResponse {
+  success: string;
+  payData: PaymentData;
+}
+
+declare global {
+  interface Window {
+    PhonePeCheckout: any;
+  }
+}
 
 export default function UploadPage() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -14,6 +33,50 @@ export default function UploadPage() {
   // const [fileUploadLoading, setFileUploadLoading] = useState(false);
   // const [isLoading, setIsLoading] = useState(false);
   // const router = useRouter();
+  const [res, setRes] = useState<PaymentResponse>();
+
+  const generateMerchantOrderId = (): string => {
+    const randomStr = Math.random().toString(36).substring(2, 10);
+    const timestamp = Date.now();
+    return `ORD-${randomStr}-${timestamp}`;
+  };
+
+  function callback(response: string) {
+    if (response === "USER_CANCEL") {
+      /* Add merchant's logic if they have any custom thing to trigger on UI after the transaction is cancelled by the user*/
+      console.log("Transaction cancelled by user");
+      return;
+    } else if (response === "CONCLUDED") {
+      console.log("Transaction concluded successfully");
+      /* Add merchant's logic if they have any custom thing to trigger on UI after the transaction is in terminal state*/
+      return;
+    }
+  }
+
+  const handlePayment = async () => {
+    const merchantOrderId = generateMerchantOrderId();
+    try {
+      const ApiRes = await axios.post("/api/phonepe/createPayment", {
+        merchantOrderId: merchantOrderId,
+        amount: 100,
+      });
+      console.log("Response:", ApiRes.data);
+      setRes(ApiRes.data as PaymentResponse);
+
+      if (window && window.PhonePeCheckout && window.PhonePeCheckout.transact) {
+        console.log("PhonePeCheckout is available", window.PhonePeCheckout);
+        console.log("res : ", res);
+        console.log("response : ", ApiRes.data);
+        window.PhonePeCheckout.transact({
+          tokenUrl: ApiRes.data?.payData.redirectUrl,
+          callback,
+          type: "IFRAME",
+        });
+      }
+    } catch (err: any) {
+      console.error("Axios error:", err.response?.data || err.message);
+    }
+  };
 
   async function handleInitialUpload(evt: React.FormEvent) {
     // setIsLoading(true);
@@ -21,11 +84,24 @@ export default function UploadPage() {
     if (!fileInputRef.current?.files?.[0]) return;
     if (fileInputRef.current?.files?.[0].type != "application/pdf") {
       setFileUploadIsInvalid(true);
-      
       setFileUploadIsInvalid(false);
     }
     // setShowFileSpecsForm(true);
+
+
+
+    handlePayment();
   }
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://mercury.phonepe.com/web/bundle/checkout.js";
+    script.async = true;
+    script.onload = () => {
+      console.log("PhonePe script loaded", window.PhonePeCheckout);
+    };
+    document.body.appendChild(script);
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#F7F5EF]">
